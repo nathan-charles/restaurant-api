79772e34fe53354187459c1d38240f5e530c18e1
Nathan Charles <nathan@nathancharles.com>
2020-07-09 17:42:46 -0500

    Update error-handler.js

--- src/helpers/error-handler.js
+++ src/helpers/error-handler.js
@@ -11,7 +11,7 @@ function errorHandler(err, req, res, next) {
 
   if (err.name === 'UnauthorizedError') {
     // jwt authentication error
-    return res.status(401).json({ message: 'Invalid Token' });
+    return res.status(401).json({ message: err.message });
   }
 
   // default to 500 server error
