298f06e1fb8ea4f69a85852dff20f03122219ade
Nathan Charles <nathan@nathancharles.com>
2020-07-13 14:34:02 -0500

    Update db.js

--- src/helpers/db.js
+++ src/helpers/db.js
@@ -2,6 +2,7 @@ import mongoose from 'mongoose';
 
 import User from '../users/user.model';
 import Restaurant from '../restaurants/restaurant.model';
+import Review from '../reviews/review.model';
 
 const connectionOptions = {
   useCreateIndex: true,
@@ -16,4 +17,5 @@ mongoose.Promise = global.Promise;
 export default {
   User,
   Restaurant,
+  Review,
 };
